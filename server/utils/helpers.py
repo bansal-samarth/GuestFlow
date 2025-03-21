6edd35e9132d01a3bebc45067a2cec0caae5095b
Samarth Bansal <work.samarthbansal@gmaiil.com>
2025-03-22 03:02:43 +0530

    Updated QR code scanning

--- server/utils/helpers.py
+++ server/utils/helpers.py
@@ -29,8 +29,8 @@ def save_photo(photo_data):
         s3_client = boto3.client(
             's3',
             region_name=AWS_REGION,
-            aws_access_key_id="AKIARZ5BNBDR4JBL6D63",
-            aws_secret_access_key="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"
+            aws_access_key_id="aws_access_key_id",
+            aws_secret_access_key="aws_secret_access_key"
         )
         
         # Create in-memory file-like object
